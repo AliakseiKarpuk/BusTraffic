9cd1c41bc6368b7ddf8fbad14cc39e5887498fb9
rodionfisenko <minsk@joingo.com>
2021-07-19 23:39:00 +0300

    add bus

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -3,33 +3,29 @@ import lombok.SneakyThrows;
 import java.util.Date;
 import java.util.concurrent.Semaphore;
 
-public class Bus implements Runnable{
+public class Bus implements Runnable {
 
     private int busNumber;
-    private final int countOnBusStop = 5;
-    Date date = new Date();
+    private static int countOnBusStop = 2;
+    public static final Semaphore semaphore = new Semaphore(countOnBusStop, true);
 
     public Bus(int busNumber) {
         this.busNumber = busNumber;
     }
 
-    public static Semaphore countBusOnStation(int countBusOnStop){
-        return new Semaphore(countBusOnStop, true);
-    }
-
     @Override
     public void run() {
         try {
-            countBusOnStation(countOnBusStop).acquire();
+            semaphore.acquire();
 
-            System.out.printf("Автобус №" + busNumber + " подъехал на остановку." + date.toString() + "\n");
+            System.out.println("Автобус №" + busNumber + " подъехал на остановку." + new Date().toString() + "\n");
             Thread.sleep(3000);
 
         } catch (InterruptedException e) {
             throw new Error();
         }
-        countBusOnStation(countOnBusStop).release();
-        System.out.printf("Автобус №" + busNumber + " покинул остановку." + date.toString() + "\n");
+        semaphore.release();
+        System.out.println("Автобус №" + busNumber + " покинул остановку." + new Date().toString() + "\n");
     }
 
     @SneakyThrows
